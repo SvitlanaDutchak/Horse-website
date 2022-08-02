d0191af78f0bdd195d9c5d1296e7591a06ced4df
“SvitlanaDutchak” <kaneki.ken123777@gmail.com>
2022-08-02 22:18:09 +0300

    changed slider

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
-$(function(){
-  $('.slider').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 1,
-    autoplay: false,
-    slidesToScroll: 1,
-    responsive: [{
-        breakpoint: 1000,
-        settings: {
-          slidesToShow: 2,
-          arrows: false
-        }
-      },
-      {
-        breakpoint: 550,
-        settings: {
-          slidesToShow: 1,
-          arrows: false
-        }
-      }
-    ]
-  });
+// $(function(){
+//   $('.slider').slick({
+//     dots: true,
+//     infinite: false,
+//     speed: 300,
+//     slidesToShow: 1,
+//     autoplay: false,
+//     slidesToScroll: 1,
+//     responsive: [{
+//         breakpoint: 1000,
+//         settings: {
+//           slidesToShow: 2,
+//           arrows: false
+//         }
+//       },
+//       {
+//         breakpoint: 550,
+//         settings: {
+//           slidesToShow: 1,
+//           arrows: false
+//         }
+//       }
+//     ]
+//   });
   
 
-})
+// })
