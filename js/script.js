4a9382860e5d2ce645183d42e04c8439d1750bbb
“SvitlanaDutchak” <kaneki.ken123777@gmail.com>
2022-08-02 20:14:02 +0300

    added slider

--- js/script.js
+++ js/script.js
@@ -1,170 +1,27 @@
-const mapStyles = [{
-  "elementType": "geometry",
-  "stylers": [{
-    "color": "#f5f5f5"
-  }]
-},
-{
-  "elementType": "labels.icon",
-  "stylers": [{
-    "visibility": "off"
-  }]
-},
-{
-  "elementType": "labels.text.fill",
-  "stylers": [{
-    "color": "#616161"
-  }]
-},
-{
-  "elementType": "labels.text.stroke",
-  "stylers": [{
-    "color": "#f5f5f5"
-  }]
-},
-{
-  "featureType": "administrative.country",
-  "elementType": "geometry.stroke",
-  "stylers": [{
-    "color": "#ac6c6c"
-  }]
-},
-{
-  "featureType": "administrative.land_parcel",
-  "elementType": "labels.text.fill",
-  "stylers": [{
-    "color": "#bdbdbd"
-  }]
-},
-{
-  "featureType": "poi",
-  "elementType": "geometry",
-  "stylers": [{
-    "color": "#eeeeee"
-  }]
-},
-{
-  "featureType": "poi",
-  "elementType": "labels.text.fill",
-  "stylers": [{
-    "color": "#757575"
-  }]
-},
-{
-  "featureType": "poi.park",
-  "elementType": "geometry",
-  "stylers": [{
-    "color": "#e5e5e5"
-  }]
-},
-{
-  "featureType": "poi.park",
-  "elementType": "geometry.fill",
-  "stylers": [{
-    "color": "#dedede"
-  }]
-},
-{
-  "featureType": "poi.park",
-  "elementType": "labels.text.fill",
-  "stylers": [{
-    "color": "#9e9e9e"
-  }]
-},
-{
-  "featureType": "road",
-  "elementType": "geometry",
-  "stylers": [{
-    "color": "#ffffff"
-  }]
-},
-{
-  "featureType": "road.arterial",
-  "elementType": "labels.text.fill",
-  "stylers": [{
-    "color": "#757575"
-  }]
-},
-{
-  "featureType": "road.highway",
-  "elementType": "geometry",
-  "stylers": [{
-    "color": "#dadada"
-  }]
-},
-{
-  "featureType": "road.highway",
-  "elementType": "labels.text.fill",
-  "stylers": [{
-    "color": "#616161"
-  }]
-},
-{
-  "featureType": "road.local",
-  "elementType": "labels.text.fill",
-  "stylers": [{
-    "color": "#9e9e9e"
-  }]
-},
-{
-  "featureType": "transit.line",
-  "elementType": "geometry",
-  "stylers": [{
-    "color": "#e5e5e5"
-  }]
-},
-{
-  "featureType": "transit.station",
-  "elementType": "geometry",
-  "stylers": [{
-    "color": "#eeeeee"
-  }]
-},
-{
-  "featureType": "water",
-  "elementType": "geometry",
-  "stylers": [{
-    "color": "#c9c9c9"
-  }]
-},
-{
-  "featureType": "water",
-  "elementType": "geometry.fill",
-  "stylers": [{
-    "color": "#c7c7c7"
-  }]
-},
-{
-  "featureType": "water",
-  "elementType": "labels.text.fill",
-  "stylers": [{
-    "color": "#9e9e9e"
-  }]
-}
-];
+$(function(){
+  $('.slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: false,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+          arrows: false
+        }
+      }
+    ]
+  });
+  
 
-function initMap() {
-const map = new google.maps.Map(document.getElementById("map"), {
-  center: {
-    lat: 50.45768752365884,
-    lng: 30.514808641267884
-  },
-  zoom: 12,
-  styles: mapStyles,
-  disableDefaultUI: true
-});
-
-const marker = "img/pin.svg";
-
-new google.maps.Marker({
-  position: {
-    lat: 50.472239709890815,
-    lng: 30.631544826103312
-  },
-  map,
-  title: "OUR LOCATION",
-  icon: marker
-});
-}
-
-window.initMap = initMap;
+})
