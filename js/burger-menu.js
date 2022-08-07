2755c7edddd9512a8a5b32963ed454cd2eca8936
Fushtei Sviatoslav <fuschtei@gmail.com>
2022-08-07 16:37:04 +0300

    add footer and hero section

---
+++ js/burger-menu.js
@@ -0,0 +1,27 @@
+let buttonMenu = document.querySelector(".menu-button");
+let darkPage = document.querySelector(".dark-page");
+let headerList = document.querySelector("#header-ul-menu");
+let close = document.querySelector(".close-window");
+let body = document.querySelector("body");
+let headerListLi = document.querySelectorAll(".header__link");
+buttonMenu.addEventListener("click", () => {
+    headerListLi.forEach(elem => {
+        elem.classList.add("dark-page-list-style");
+    });
+    headerList.classList.remove("display-none");
+    headerList.classList.remove("header__list");
+    headerList.classList.add("dark-page-list");
+    darkPage.classList.remove("display-none");
+    body.classList.add("overflow");
+})
+
+close.addEventListener("click", () => {
+    headerListLi.forEach(elem => {
+        elem.classList.remove("dark-page-list-style");
+    });
+    headerList.classList.add("header__list");
+    headerList.classList.add("ul-display-none");
+    darkPage.classList.add("display-none");
+    headerList.classList.remove("dark-page-list");
+    body.classList.remove("overflow");
+})
